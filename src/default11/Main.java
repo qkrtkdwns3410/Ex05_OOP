ff69502fc2d17d5810cf3ef17825a21622a3edf1
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-07-25 13:19:46 +0900

    7-23

--- src/default11/Main.java
+++ src/default11/Main.java
@@ -6,11 +6,47 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
 
 
     }
 }
+
+class Unit {
+    int x, y;
+
+    void move(int x, int y) {
+
+    }
+
+    void stop() {
+
+    }
+
+}
+class Marine extends Unit{
+
+    void stimPack() {
+    }
+
+}
+class Tank extends Unit{
+
+    void changeMode() {
+    }
+
+}
+
+class DropShip extends Unit {
+    void load() {
+
+    }
+
+    void unload() {
+
+    }
+
+}
+
 
 
 
