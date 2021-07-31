4a9a6394faf4d5b578d66c45d1adb2dc501edafa
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-07-31 13:36:25 +0900

    7-23

--- src/default11/Main.java
+++ src/default11/Main.java
@@ -1,27 +1,16 @@
 package default11;
 
 
+
 import java.util.Scanner;
-import java.util.Stack;
 
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        while (num-- > 0) {
-            for (int i = 0; i < num; i++) {
-                String[] strArr = sc.nextLine().split(" ");
-                for (int j = strArr.length - 1; j >= 0; j--) {
-                    System.out.printf("%s ",strArr[j]);
-                }
-                System.out.println();
-            }
-        }
-
-
-
     }
+
 }
 
 
