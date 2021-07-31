2c8bbf297f0df16d883dd0e1d162ab5c284fca1d
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-07-31 17:34:32 +0900

    7-23

--- src/default11/Main.java
+++ src/default11/Main.java
@@ -1,17 +1,34 @@
 package default11;
 
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-    }
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        ArrayList<String> strList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            str = in.readLine();
+            if (str == null) {
+                strList.add("");
+            } else {
+                strList.add(str);
+            }
+        }
+        for (String strr : strList) {
+            System.out.println(strr);
+        }
+    }
 }
+
+
 
 
 
