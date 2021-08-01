3865c36a9085829f1f11868eaea047bd2d8ab36c
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-08-01 12:22:08 +0900

    7-23

--- src/default11/Main.java
+++ src/default11/Main.java
@@ -1,6 +1,7 @@
 package default11;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,17 +9,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int K = sc.nextInt();
+        int M = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int j = 0; j < N; j++) {
+            for (int i = 0; i < M; i++) {
 
-        int ans = fac(N) / (fac(N - K) * fac(K));
-        System.out.println(ans);
-    }
-
-    static int fac(int num) {
-        if (num == 0) {
-            return 1;
+            }
         }
-        return num * fac(num - 1);
+
     }
 
 }
