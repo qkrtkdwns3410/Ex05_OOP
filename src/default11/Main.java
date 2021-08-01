51634b7084f39c05967fcd42d580d3d595a46238
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-08-01 14:55:07 +0900

    7-23

--- src/default11/Main.java
+++ src/default11/Main.java
@@ -1,25 +1,17 @@
 package default11;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        ArrayList<Integer> arr = new ArrayList<>();
-        for (int j = 0; j < N; j++) {
-            for (int i = 0; i < M; i++) {
-
-            }
-        }
 
     }
 
 }
+
 
 
 
