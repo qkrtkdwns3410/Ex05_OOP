8671122db97cce45fffd6f804587365a71754b31
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-08-01 15:44:16 +0900

    7-23

--- src/default11/Main.java
+++ src/default11/Main.java
@@ -6,8 +6,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
 
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int ans = sc.nextInt();
+            char[] ch = String.valueOf(ans).toCharArray();
+            if (ans == 0) {
+                break;
+            }
+            for (int i = 0; i < ans/2; i++) {
+                if(ch[0]==ch[])
+            }
+        }
     }
 
 }
